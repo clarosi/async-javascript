9f37221eb464b2e2691c1716e3fb4fa6c0266fc2
Ian Realista Claros <claroia@mliph.manulife.com.ph>
2019-07-08 09:35:41 +0800

    exam

--- exam.js
+++ exam.js
@@ -1,1 +1,24 @@
-console.log('sdfsdf');
+const addBtn = ({ id, value, name }) => {
+  const ele = document.createElement('input');
+
+  ele.setAttribute('type', 'button');
+  ele.setAttribute('id', id);
+  ele.setAttribute('value', value);
+  ele.setAttribute('name', name);
+  ele.setAttribute('style', 'padding: 10px; margin: 0 15px 10px 0');
+  ele.setAttribute('onclick', `btnClick(${id})`);
+
+  const parent = document.getElementById('root');
+  parent.appendChild(ele);
+};
+
+const btnClick = num => {
+  alert(`btn ${num}`);
+};
+
+let i = 1;
+while (i <= 20) {
+  const obj = { id: i, value: `btn #${i}`, name: `btn${i}` };
+  addBtn(obj);
+  i++;
+}
