72fe62e12c47640ce24f8997741aa628365b315c
Ian Realista Claros <claroia@mliph.manulife.com.ph>
2019-07-10 15:56:11 +0800

    07/10/2019

--- exam.js
+++ exam.js
@@ -1,24 +1,26 @@
-const addBtn = ({ id, value, name }) => {
+const addBtn = ({ id, value }) => {
   const ele = document.createElement('input');
 
   ele.setAttribute('type', 'button');
   ele.setAttribute('id', id);
   ele.setAttribute('value', value);
-  ele.setAttribute('name', name);
-  ele.setAttribute('style', 'padding: 10px; margin: 0 15px 10px 0');
   ele.setAttribute('onclick', `btnClick(${id})`);
 
-  const parent = document.getElementById('root');
-  parent.appendChild(ele);
+  const con = document.getElementById('button-container');
+  con.appendChild(ele);
 };
 
 const btnClick = num => {
-  alert(`btn ${num}`);
+  alert(num);
 };
 
-let i = 1;
-while (i <= 20) {
-  const obj = { id: i, value: `btn #${i}`, name: `btn${i}` };
-  addBtn(obj);
-  i++;
-}
+const addButtons = () => {
+  let i = 1;
+  while (i <= 20) {
+    const obj = { id: i, value: `Button #${i}` };
+    addBtn(obj);
+    i++;
+  }
+};
+
+addButtons();
